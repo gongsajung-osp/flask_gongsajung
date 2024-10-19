0e90a13917bb08e4e1d318e23f125f5bc8c7be2c
methal <handsson@ewhain.net>
2024-10-19 13:08:44 +0900

    organize files

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# flask_gongsajung
+
--- file2/static/main.js
+++
@@ -1,6 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn');
-const menu = document.querySelector('.navbar__menu');
-
-toggleBtn.addEventListener('click', () => {
-    menu.classList.toggle('show'); // 클릭 시 'show' 클래스를 추가/제거하여 메뉴 보이기/숨기기
-});
